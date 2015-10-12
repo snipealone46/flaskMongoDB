d7a446c83b2a213c9c479c0f96c3f075fde5f571
Eric Yang <eric.shaohuiyang@gmail.com>
2015-10-11 23:56:40 -0700

    Trying out the moment js
    *it is used to add or fetch a time stamp
    *can be used in post or comment

--- server.py
+++ server.py
@@ -1,20 +1,23 @@
 from flask import Flask, render_template
 from flask.ext.bootstrap import Bootstrap
 from flask.ext.script import Manager
+from datetime import datetime
+from flask.ext.moment import Moment
 
 app = Flask(__name__)
 Bootstrap(app)
 manager = Manager(app)
+moment = Moment(app)
 
 
 @app.route('/')
 def index():
-    return render_template('index.html')
+    return render_template('index.html', current_time=datetime.utcnow())
 
 
 @app.route('/user/<name>')
 def user(name):
-    return render_template('index.html', name=name)
+    return render_template('index.html', name=name, current_time=datetime.utcnow())
 
 
 if __name__ == '__main__':
