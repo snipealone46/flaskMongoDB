f26299c1fba3aad3d4b42d39cade318f25597477
Eric Yang <eric.shaohuiyang@gmail.com>
2015-10-21 22:33:07 -0700

    Linked login form with flask
    *bootstrap now works with wtforms
    *use flask.ext.wtf import Form
    *easier than from wtforms import Forms
    *nextup: link up the database

--- server.py
+++ server.py
@@ -1,21 +1,41 @@
-from flask import Flask, render_template
+from flask import Flask, render_template, session, redirect, url_for
 from flask.ext.bootstrap import Bootstrap
 from flask.ext.script import Manager
+from datetime import datetime
+from flask.ext.moment import Moment
+from flask.ext.wtf import Form
+from wtforms import SubmitField, StringField, PasswordField, validators
 
 app = Flask(__name__)
 bootstrap = Bootstrap(app)
 app.config['SECRET_KEY'] = 'hard to guess string'
 manager = Manager(app)
+# moment = Moment(app)
 
 
-@app.route('/')
-def index():
-    return render_template('index.html')
+class ID(Form):
+    ID = StringField('username', [validators.InputRequired(message='please enter your username')])
 
 
-@app.route('/user/<name>')
-def user(name):
-    return render_template('index.html', name=name)
+class LoginForm(ID):
+    password = PasswordField('password')
+    submit = SubmitField('Submit')
+
+
+class RegisterForm(ID):
+    emailValMessage = "Please enter a valid email address"
+    userEmail = StringField('email', [validators.InputRequired(message=emailValMessage), validators.email(message=emailValMessage)])
+    newPassword = PasswordField('New Password', [validators.InputRequired(), validators.EqualTo('confirm', message='Passwords must match')])
+    submit = SubmitField('Submit')
+
+
+@app.route('/', methods=['GET', 'POST'])
+def forum():
+    login = LoginForm()
+    if login.validate_on_submit():
+        session['user'] = login.ID.data
+        return redirect(url_for('forum'))
+    return render_template('index.html', login=login, user=session.get('user'))
 
 
 if __name__ == '__main__':
