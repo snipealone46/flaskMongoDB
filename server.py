48e4fc21c852b83e8269a8a2736c132fab0b6939
Eric Yang <eric.shaohuiyang@gmail.com>
2015-10-27 23:55:43 -0700

    initialize connectiong between server and database
    *Now will check with database and check username if it exist
    *if it does not exist, will flash a message saying so

--- server.py
+++ server.py
@@ -1,16 +1,35 @@
-from flask import Flask, render_template, session, redirect, url_for
+from flask import Flask, flash, render_template, session, redirect, url_for
 from flask.ext.bootstrap import Bootstrap
 from flask.ext.script import Manager
 from datetime import datetime
 from flask.ext.moment import Moment
 from flask.ext.wtf import Form
 from wtforms import SubmitField, StringField, PasswordField, validators
+from pymongo import MongoClient
 
 app = Flask(__name__)
 bootstrap = Bootstrap(app)
 app.config['SECRET_KEY'] = 'hard to guess string'
 manager = Manager(app)
+client = MongoClient()
+loginInfo = client.user.login
+
 # moment = Moment(app)
+
+
+class LoginDB(object):
+    def __init__(self, username, password):
+        self.login = loginInfo
+        self.ID = username
+        self.password = password
+
+# checkexist(): if exist return true otherwise return false
+    def checkexist(self):
+        findmatch = self.login.find_one({"ID": "%s" % self.ID})
+        if findmatch:
+            return True
+        else:
+            return False
 
 
 class ID(Form):
@@ -33,7 +52,11 @@ class RegisterForm(ID):
 def forum():
     login = LoginForm()
     if login.validate_on_submit():
-        session['user'] = login.ID.data
+        inputinfo = LoginDB(login.ID.data, login.password.data)
+        if inputinfo.checkexist():
+            session['user'] = login.ID.data
+        else:
+            flash('The username does not exist!')
         return redirect(url_for('forum'))
     return render_template('index.html', login=login, user=session.get('user'))
 
